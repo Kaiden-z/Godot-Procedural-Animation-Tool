bf21d83f2834f06f9ed8a44b9e6ddb59ab82a5c3
Kaiden Zapanta <kaidenzapanta@gmail.com>
2024-12-11 20:59:27 -0600

    binary built

--- akikproj/scripts/Caterpillar.cs
+++ akikproj/scripts/Caterpillar.cs
@@ -4,21 +4,22 @@ using System;
 public partial class Caterpillar : Node3D
 {
 	[Export] public float speed = 2f;
+    [Export] public float amplitude = 5f;
     private Vector3 startPosition;
 
     public override void _Ready()
     {
         // Store the initial position of the node
-        startPosition = new Vector3(0, 1, 7f);
+        startPosition = Position;
     }
 
     public override void _Process(double delta)
     {
         // Calculate the new X and Z positions for the squiggly movement
-        float x = Mathf.Sin(Time.GetTicksMsec() / 1000.0f * speed) * 7;
-        float z = Mathf.Cos(Time.GetTicksMsec() / 1000.0f * speed) * 7;
+        float x = Mathf.Sin(Time.GetTicksMsec() / 1000.0f * speed) * amplitude + startPosition.X;
+        float z = Mathf.Cos(Time.GetTicksMsec() / 1000.0f * speed) * amplitude + startPosition.Z;
 
         // Set the node's position, maintaining the initial Y position
-		GlobalPosition = new Vector3(x, 2, z);
+		Position = new Vector3(x, startPosition.Y, z);
     }
 }
